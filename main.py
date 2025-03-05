094f94989e5c8ca0b5149d3559e7e3d63149abf8
Duued <66633587+Duued@users.noreply.github.com>
2025-03-05 12:01:38 -0500

    Implement isOdd

---
+++ main.py
@@ -0,0 +1,25 @@
+def isOdd(input: int) -> bool:
+    if input == 0:
+        return False
+    elif input == 1:
+        return True
+    elif input == 2:
+        return False
+    elif input == 3:
+        return True
+    elif input == 4:
+        return False
+    elif input == 5:
+        return True
+    elif input == 6:
+        return False
+    elif input == 7:
+        return True
+    elif input == 8:
+        return False
+    elif input == 9:
+        return True
+    elif input == 10:
+        return False
+    else:
+        raise ValueError("I do not know. Please submit an issue report.")
